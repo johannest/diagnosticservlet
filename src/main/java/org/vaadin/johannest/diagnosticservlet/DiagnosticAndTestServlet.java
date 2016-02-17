0796a834ffe373776521916b465965dea778b1b2
Johannes Tuikkala <johannes@vaadin.com>
2016-02-17 13:39:55 +0200

    upgraded grid support

--- src/main/java/org/vaadin/johannest/diagnosticservlet/DiagnosticAndTestServlet.java
+++ src/main/java/org/vaadin/johannest/diagnosticservlet/DiagnosticAndTestServlet.java
@@ -1,16 +1,15 @@
 package org.vaadin.johannest.diagnosticservlet;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.vaadin.server.DeploymentConfiguration;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinServletService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Marcus Hellberg (marcus@vaadin.com) Further modified by Johannes
@@ -25,8 +24,8 @@ public class DiagnosticAndTestServlet extends VaadinServlet {
 	@Override
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		Logger.getLogger(DiagnosticAndTestServlet.class.getName()).info(
-				request.getRequestURI());
+//		Logger.getLogger(DiagnosticAndTestServlet.class.getName()).info(
+//				request.getRequestURI());
 
 		super.service(request, response);
 	}
--- src/main/java/org/vaadin/johannest/diagnosticservlet/DiagnosticAndTestServletSession.java
+++ src/main/java/org/vaadin/johannest/diagnosticservlet/DiagnosticAndTestServletSession.java
@@ -3,7 +3,12 @@ package org.vaadin.johannest.diagnosticservlet;
 import com.vaadin.server.ClientConnector;
 import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinSession;
+import com.vaadin.server.communication.data.RpcDataProviderExtension;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Grid;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiagnosticAndTestServletSession extends VaadinSession {
 	private static final long serialVersionUID = 4596901275146146127L;
@@ -12,13 +17,40 @@ public class DiagnosticAndTestServletSession extends VaadinSession {
 		super(service);
 	}
 
+	private Map<Class<? extends ClientConnector>, Integer> sequences = new HashMap<Class<? extends ClientConnector>, Integer>();
+
+	@SuppressWarnings("deprecation")
 	@Override
 	public String createConnectorId(ClientConnector connector) {
+		String connectorId = "";
 		if (connector instanceof Component) {
 			Component component = (Component) connector;
-			return component.getId() == null ? super
+			connectorId = component.getId() == null ? super
 					.createConnectorId(connector) : component.getId();
+		} else if (connector.getClass().equals(RpcDataProviderExtension.class)) {
+			connectorId = "rpcdpext-" + nextId(connector.getClass());
+		} else if (connector.getClass().equals(Grid.SingleSelectionModel.class)) {
+			connectorId = "singleselect-grid-" + nextId(connector.getClass());
+		} else if (connector.getClass().equals(Grid.MultiSelectionModel.class)) {
+			connectorId = "multiselect-grid-" + nextId(connector.getClass());
+		} else {
+			connectorId = super.createConnectorId(connector);
 		}
-		return super.createConnectorId(connector);
+
+//		Logger.getLogger(DiagnosticAndTestServletSession.class.getName()).info(
+//				"Created connector id " + connectorId + " for "
+//						+ connector.getClass());
+
+		return connectorId;
 	}
+
+	private int nextId(Class<? extends ClientConnector> c) {
+		Integer nextid = 0;
+		if (sequences.get(c)!=null) {
+			nextid = sequences.get(c);
+		}
+		sequences.put(c, nextid+1);
+		return nextid;
+	}
+
 }
